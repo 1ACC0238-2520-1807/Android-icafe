86804944b385927c8e16dd0db8be1a904507789c
Jass <jasminurrutia30@gmail.com>
2025-10-08 01:00:26 -0500

    Add files via upload

---
+++ app/src/main/java/com/example/icafe/ui/theme/Color.kt
@@ -0,0 +1,21 @@
+package com.example.icafe.ui.theme
+
+import android.widget.Button
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val ColorIcafe = Color(0xFF8F4C32)
+
+val BrownDark = Color(0xFF6D4C41) // Para títulos y botones oscuros
+val BrownMedium = Color(0xFF8D6E63) // Para fondos de diálogo y cabeceras
+val OliveGreen = Color(0xFF79745B) // Para botones principales como "Agregar" y "Ver más"
+val Peach = Color(0xFFF5E0D8) // Para botones de edición y fondos de diálogo claros
+val LightGrayBackground = Color(0xFFE0E0E0) // Para los campos de texto
+val OffWhiteBackground = Color(0xFFFFF8F5) // Fondo general de las pantallas
